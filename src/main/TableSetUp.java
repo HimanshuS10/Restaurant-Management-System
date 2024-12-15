fb1665e06f415281b42a1d3a118aa507869c3ea6
Himanshu Seervi <himseervi@gmail.com>
2024-12-14 23:48:11 -0500

    Added the square (tables) to the table set up

--- src/main/TableSetUp.java
+++ src/main/TableSetUp.java
@@ -1,15 +1,11 @@
 package main;
 
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-
 import javax.swing.*;
+import java.awt.*;
 
 public class TableSetUp extends JFrame {
 
     public TableSetUp() {
-        // this.userList = User;
-
         // Sets the title of the window
         setTitle("Restaurant Management System - Table Set Up");
         setSize(800, 600);
@@ -17,7 +13,7 @@ public class TableSetUp extends JFrame {
         setLocationRelativeTo(null); // Center the window
         setResizable(false);
 
-        // Add the signup page to the frame
+        // Add the setup page to the frame
         getContentPane().add(TableSetUpPage());
 
         setVisible(true); // Make the frame visible
@@ -25,16 +21,35 @@ public class TableSetUp extends JFrame {
 
     public JPanel TableSetUpPage() {
         JPanel tableSetUpPage = new JPanel();
-        
-        JLabel asd = new JLabel("Table Set Up Page");
-        tableSetUpPage.add(asd);
-    
-        tableSetUpPage.getRootPane().add(new TablesShapes());
+        tableSetUpPage.setLayout(null); // Use absolute positioning
 
-        tableSetUpPage.setLayout(null);
+        JLabel title = new JLabel("Table Set Up Page");
+        title.setBounds(10, 10, 200, 30); // Position the label
+        tableSetUpPage.add(title);
 
-        
+        // Add the TablesShapes component
+        TablesShapes tablesShapes = new TablesShapes();
+        tablesShapes.setBounds(50, 50, 700, 500); // Set the bounds of the drawing area
+        tableSetUpPage.add(tablesShapes);
+
         return tableSetUpPage;
     }
 
+    public static void main(String[] args) {
+        new TableSetUp();
+    }
+}
+
+class TablesShapes extends JComponent {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // Ensure parent component is painted
+        Graphics2D g2 = (Graphics2D) g;
+
+        // Draw a square
+        g2.setColor(Color.BLUE);
+        g2.fillRect(100, 100, 100, 100); // Fill a square
+        g2.setColor(Color.BLACK);
+        g2.drawRect(100, 100, 100, 100); // Draw the outline
+    }
 }
